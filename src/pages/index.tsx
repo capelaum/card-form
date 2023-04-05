86b9361b71d1187fc581ed762883d664233c28f9
capelaum <thecapellett@gmail.com>
2023-04-05 11:42:43 -0300

    ✨ home: form styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
+import { BgDesktop } from '@/assets'
+import { Button } from '@/components/Button'
+import { Label } from '@/components/Label'
+import { TextInput } from '@/components/TextInput'
+import {
+  DateField,
+  DateInputs,
+  FormRow,
+  FormWrapper,
+  HomeWrapper,
+  ImageWrapper,
+} from '@/styles/home'
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -6,9 +19,43 @@ export default function Home() {
       <Head>
         <title>Card Form | Frontend Mentor</title>
       </Head>
-      <main>
-        <h1>Hello Card Form</h1>
-      </main>
+      <HomeWrapper>
+        <ImageWrapper>
+          <Image src={BgDesktop} alt="Background with a violet gray gradient" />
+        </ImageWrapper>
+
+        <FormWrapper>
+          <TextInput
+            label="Cardholder Name"
+            id="name"
+            placeholder="e.g. Jane Appleseed"
+          />
+
+          <TextInput
+            label="Card Number"
+            id="number"
+            placeholder="e.g. 1234 5678 9123 0000"
+          />
+
+          <FormRow>
+            <DateField>
+              <Label text="Exp. Date (MM/YY)" htmlFor="month" />
+              <DateInputs>
+                <TextInput id="month" placeholder="MM" />
+                <TextInput id="year" placeholder="YY" />
+              </DateInputs>
+            </DateField>
+
+            <TextInput
+              label="CVC"
+              id="cvc"
+              placeholder="e.g. 1234 5678 9123 0000"
+            />
+          </FormRow>
+
+          <Button type="submit">Confirm</Button>
+        </FormWrapper>
+      </HomeWrapper>
     </>
   )
 }
---
+++ src/styles/home.ts
@@ -0,0 +1,52 @@
+import { styled } from './stitches.config'
+
+export const HomeWrapper = styled('main', {
+  display: 'flex',
+  height: '100%',
+  overflow: 'hidden',
+})
+
+export const ImageWrapper = styled('div', {
+  height: '100vh',
+  width: '30%',
+  minWidth: '300px',
+
+  img: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'cover',
+  },
+})
+
+export const FormWrapper = styled('form', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  gap: '$6',
+  margin: '$10 $8',
+
+  maxWidth: '400px',
+
+  button: {
+    marginTop: '$4',
+  },
+})
+
+export const DateField = styled('fieldset', {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  border: 'none',
+  gap: '$1',
+})
+
+export const DateInputs = styled('div', {
+  display: 'flex',
+  gap: '$2',
+})
+
+export const FormRow = styled('div', {
+  width: '100%',
+  display: 'flex',
+  gap: '$6',
+})
