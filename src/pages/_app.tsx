358e7835cddf454c2549f13072919a2c8997a500
capelaum <thecapellett@gmail.com>
2023-04-05 11:42:15 -0300

    🐛 fix: font load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
-import { Space_Grotesk } from 'next/font/google'
-
-const spaceGrotesk = Space_Grotesk({
-  display: 'swap',
-  weight: ['400', '500', '700'],
-  subsets: ['latin'],
-})
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} className={spaceGrotesk.className} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
