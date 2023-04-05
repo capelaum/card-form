33edb7c3df0127f96ed5ad1516c5849c1a503d69
capelaum <thecapellett@gmail.com>
2023-04-04 22:14:10 -0300

    🍱  export assets

---
+++ src/assets/index.tsx
@@ -0,0 +1,8 @@
+import CardBack from './images/bg-card-back.png'
+import CardFront from './images/bg-card-front.png'
+import CardLogo from './images/bg-card-logo.png'
+import BgDesktop from './images/bg-main-desktop.png'
+import BgMobile from './images/bg-main-mobile.png'
+import IconComplete from './images/icon-complete.svg'
+
+export { IconComplete, CardLogo, CardBack, CardFront, BgDesktop, BgMobile }
