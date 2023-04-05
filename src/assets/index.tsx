35d0a57f6b1852a3cc0c64af75f5804c6c35c8bd
capelaum <thecapellett@gmail.com>
2023-04-05 11:41:58 -0300

    🐛 fix: assets file path

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,8 +1,8 @@
 import CardBack from './images/bg-card-back.png'
 import CardFront from './images/bg-card-front.png'
-import CardLogo from './images/bg-card-logo.png'
 import BgDesktop from './images/bg-main-desktop.png'
 import BgMobile from './images/bg-main-mobile.png'
+import CardLogo from './images/card-logo.svg'
 import IconComplete from './images/icon-complete.svg'
 
 export { IconComplete, CardLogo, CardBack, CardFront, BgDesktop, BgMobile }
